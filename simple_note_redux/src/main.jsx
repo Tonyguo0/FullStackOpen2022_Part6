609811ad132c926a41b6db619e4d9dbc7fe31d54
tonyguo0 <tonyguo998@gmail.com>
2026-02-21 23:57:42 +0800

    upto: 6a forwarding Redux Store to various components
    Refactor app to use Redux Provider pattern
    
    Adds react-redux dependency and restructures the application to properly use Redux Provider for state management, moving the App component to a separate file.
    
    Instead of manually subscribing to store updates, the app now uses the Provider component to make the Redux store accessible to all child components through context. This enables components to connect to Redux using hooks or the connect API.
    
    Comments out initialization code to prepare for the new component-based architecture where Redux actions will be dispatched from individual components rather than at the root level.

--- simple_note_redux/src/main.jsx
+++ simple_note_redux/src/main.jsx
@@ -1,105 +1,111 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import { ReactDOM } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import App from './App';
+import './index.css';
 import noteReducer from './reducers/noteReducer';
+
 
 const store = createStore(noteReducer);
 
-store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-        content: `the app state is in redux store`,
-        important: true,
-        id: 1
-    }
-});
 
-store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-        content: `stage changes are made with actions`,
-        important: true,
-        id: 2
-    }
-});
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-const App = () => {
-    const addNote = (event) => {
-        event.preventDefault();
-        // content of new note is obtained directly from the form input field,
-        // which is accessed through event.target.note.value, where note is the name of the input field
-        const content = event.target.note.value;
-        event.target.note.value = ``;
-        // dispatch the action for adding notes
-        store.dispatch(createNote(content));
-    };
-
-    // action creator for creating an action for adding a new note, it is used in the addNote function
-    const createNote = (content) => {
-        return {
-            type: `NEW_NOTE`,
-            payload: {
-                content,
-                important: false,
-                id: generateId()
-            }
-        };
-    };
-
-    // action creator for creating an action for toggling the importance of a note,
-    // it is used in the onClick handler of each note
-    const toggleImportanceOf = (id) => {
-        return {
-            type: 'TOGGLE_IMPORTANCE',
-            payload: {
-                id
-            }
-        };
-    };
-
-    // note's importance can be changed by clicking its name
-    const toggleImportance = (id) => {
-        store.dispatch(toggleImportanceOf(id));
-    };
-    return (
-        <div>
-            <form onSubmit={addNote}>
-                {/* input field must have a name in order to access its value */}
-                <input name="note" />
-                <br />
-                <button type="submit">add</button>
-            </form>
-
-            <ul>
-                {store.getState().map((note) => (
-                    <li
-                        key={note.id}
-                        onClick={() => toggleImportance(note.id)}
-                    >
-                        {note.content}{' '}
-                        <strong>{note.important ? 'important' : ''}</strong>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
-
-const root = createRoot(document.getElementById('root'));
-
-const renderApp = () => {
-    root.render(
-        <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <Provider store={store}>
             <App />
-        </StrictMode>
-    );
-};
+        </Provider>
+    </StrictMode>
+);
 
-renderApp();
-store.subscribe(renderApp);
+
+// renderApp();
+// store.subscribe(renderApp);
+
+
+// store.dispatch({
+//     type: 'NEW_NOTE',
+//     payload: {
+//         content: `the app state is in redux store`,
+//         important: true,
+//         id: 1
+//     }
+// });
+
+// store.dispatch({
+//     type: 'NEW_NOTE',
+//     payload: {
+//         content: `stage changes are made with actions`,
+//         important: true,
+//         id: 2
+//     }
+// });
+
+// const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+// const App = () => {
+//     const addNote = (event) => {
+//         event.preventDefault();
+//         // content of new note is obtained directly from the form input field,
+//         // which is accessed through event.target.note.value, where note is the name of the input field
+//         const content = event.target.note.value;
+//         event.target.note.value = ``;
+//         // dispatch the action for adding notes
+//         store.dispatch(createNote(content));
+//     };
+
+//     // action creator for creating an action for adding a new note, it is used in the addNote function
+//     const createNote = (content) => {
+//         return {
+//             type: `NEW_NOTE`,
+//             payload: {
+//                 content,
+//                 important: false,
+//                 id: generateId()
+//             }
+//         };
+//     };
+
+//     // action creator for creating an action for toggling the importance of a note,
+//     // it is used in the onClick handler of each note
+//     const toggleImportanceOf = (id) => {
+//         return {
+//             type: 'TOGGLE_IMPORTANCE',
+//             payload: {
+//                 id
+//             }
+//         };
+//     };
+
+//     // note's importance can be changed by clicking its name
+//     const toggleImportance = (id) => {
+//         store.dispatch(toggleImportanceOf(id));
+//     };
+//     return (
+//         <div>
+//             <form onSubmit={addNote}>
+//                 {/* input field must have a name in order to access its value */}
+//                 <input name="note" />
+//                 <br />
+//                 <button type="submit">add</button>
+//             </form>
+
+//             <ul>
+//                 {store.getState().map((note) => (
+//                     <li
+//                         key={note.id}
+//                         onClick={() => toggleImportance(note.id)}
+//                     >
+//                         {note.content}{' '}
+//                         <strong>{note.important ? 'important' : ''}</strong>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// };
+
+// const root = createRoot(document.getElementById('root'));
 
 // createRoot(document.getElementById('root')).render(
 //     <StrictMode>
